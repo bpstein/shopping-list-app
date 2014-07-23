486f7c5f156a1bed724797ba1e10b6209fef23e7
Ben Stein <bumstain@gmail.com>
2014-07-23 23:42:44 +0100

    Updated check/uncheck functionality

--- js/app.js
+++ js/app.js
@@ -1,3 +1,34 @@
 $(document).ready(function(){
 	
-})
+	//Add item in form; add item to list
+
+	//Use "enter" keystroke to submit item to list
+	function addItem() {
+		$(xxx).keydown(function (enter) {
+			console.log("typed character in list")
+			if (enter.keyCode == 13) {
+				postItem();
+			}
+		});
+	}
+
+	//Click 'x' and remove item from list
+	$(document).on("click", "cross", function(){
+		console.log("Marking item as complete");
+		$(this).closest('.li').toggleClass("checked");
+	}
+
+	//User checks off items; click 'check' and convert item to line-through state
+	$(document).on("click", "check", function(){
+		console.log("Marking item as complete");
+		$(this).closest('.li').toggleClass("checked");
+	}
+
+
+	//Enable draw functionality on post-it note
+
+});
+
+
+
+
