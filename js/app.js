743e88fcbbc95b62c8544b98d578e977a2d3cdf6
Ben Stein <bumstain@gmail.com>
2014-07-25 19:09:51 +0100

    Updated scripts for all actions

--- js/app.js
+++ js/app.js
@@ -1,16 +1,73 @@
-$(document).ready(function(){
+$(document).ready(function() {
+
+	//Add item in form, then click "submit" to add item to list
+	$('#item').click(function() {	
+		  $('#item').val("");
+    });
+
+    $('#instructions-container').on('click','input[name="add"]', function() {
+      preventDefault();
+      addItem();
+    });
+
+	//User "enter" keystroke to submit item to list
+	$('body').on('keydown','#item', function(event) {
+      if( event.which == 13) {
+        preventDefault();
+				addItem();
+      }
+  });
+
+	//Click "x" to remove item from list
+	 	$('body').on('click','.delete',function() {
+        $(this).closest("li").remove();
+   	});
+
+	//Click "check" to convert item to line-through state
+		$('body').on('click', '.complete', function() {
+        $(this).closest("li").css("text-decoration", "line-through");
+    });
+
+			function addItem(){
+        $('ul').append('<li>'+$('#itemName').val()+'<button class="delete">Delete</button><button class="complete">Complete</button></li>');
+        $('#itemName').val("");
+    	}
+
+	//Add post-it note functionality to 
 	
-	//Add item in form; add item to list
+
+});
+
+
+
+
+
+
+
+
+//Add item in form; click "submit" to add item to list
+	$(document).on("click", "#button" function(){
+
+	})
+
+
+
+
+
+	$("#button").submit(function(event) {
+  alert( "Handler for .submit() called." );
+  event.preventDefault();
+});
 
 	//Use "enter" keystroke to submit item to list
-	function addItem() {
-		$(xxx).keydown(function (enter) {
-			console.log("typed character in list")
-			if (enter.keyCode == 13) {
-				postItem();
-			}
-		});
-	}
+	// function addItem() {
+	// 	$(xxx).keydown(function (enter) {
+	// 		console.log("typed character in list")
+	// 		if (enter.keyCode == 13) {
+	// 			postItem();
+	// 		}
+	// 	});
+	// }
 
 	//Click 'x' and remove item from list
 	$(document).on("click", "cross", function(){
@@ -19,16 +76,10 @@ $(document).ready(function(){
 	}
 
 	//User checks off items; click 'check' and convert item to line-through state
-	$(document).on("click", "check", function(){
-		console.log("Marking item as complete");
-		$(this).closest('.li').toggleClass("checked");
-	}
+	// $(document).on("click", "check", function(){
+	// 	console.log("Marking item as complete");
+	// 	$(this).closest('.li').toggleClass("checked");
+	// }
 
 
 	//Enable draw functionality on post-it note
-
-});
-
-
-
-
